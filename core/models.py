3774f3a522855c73fe991d642b02835335934718
June Muoti <june.muoti@gmail.com>
2020-05-23 19:18:04 +0300

    Add products

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.shortcuts import reverse
 
 # Create your models here.
 
@@ -11,7 +12,7 @@ CATEGORY_CHOICES=(
 
 LABEL_CHOICES=(
     ('P','primary'),
-     ('S','secondary'),
+     ('S','secondary'), 
       ('D','danger')
 )
 
@@ -19,11 +20,13 @@ class Item(models.Model):
     title=models.CharField(max_length=100)
     price=models.FloatField()
     category=models.CharField(choices=CATEGORY_CHOICES,max_length=1 )
-    lavel=models.CharField(choices=LABEL_CHOICES,max_length=1 )
-
+    label=models.CharField(choices=LABEL_CHOICES,max_length=1 ,default='P ')
+    slug=models.SlugField()
     def __str__(self):
         return self.title
-
+    def get_absolute_url(self):
+        
+        return reverse("core:product", kwargs={"slug": self.slug})
 
 
 class OrderItem(models.Model):
--- core/views.py
+++ core/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from  .models import Item
+from django.views.generic import ListView,DetailView
+
 
 
 # Create your views here.
-def item_list(request):
+def products(request):
     context={
         'items':Item.objects.all()
     }
-    return render (request,"home-page.html",context)
+    return render (request,"product-page.html",context)
+class HomeView(ListView):
+    model=Item
+    template_name="home-page.html"
 
+class ItemDetailView(DetailView):
+     model=Item 
+     template_name="product.html "
+     
+def checkout(request):
+    return render(request,"checkout-page.html")
