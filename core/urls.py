7e036bd63c450620c479cf52f237a329c81c106f
June Muoti <june.muoti@gmail.com>
2020-05-25 15:29:55 +0300

    add url routing for adding and removing items from cart

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,9 @@ from .views import (
   
     checkout,
     HomeView,
-    ItemDetailView
+    ItemDetailView,
+    add_to_cart,
+    remove_from_cart
 )
 app_name='core'
 
@@ -11,4 +13,6 @@ urlpatterns = [
     path('', HomeView.as_view(),name='home'),
      path('checkout/', checkout,name='checkout'),
      path('products/<slug>/ ', ItemDetailView.as_view(),name='product'),
+     path('add-to-cart/<slug>/',add_to_cart,name='add_to_cart'),
+     path('remove-from-cart/<slug>/',remove_from_cart,name='remove_from_cart')
 ]
