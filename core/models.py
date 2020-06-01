137a1809e8eb01063254e2b75325a651d8cc73cc
June Muoti <june.muoti@gmail.com>
2020-06-01 21:29:16 +0300

    Add functions for total price,discount price,amount saved and final item price

--- core/models.py
+++ core/models.py
@@ -48,6 +48,16 @@ class OrderItem(models.Model):
 
     def  __str__(self):
         return "{} of {}.".format(self.quantity,self.item.title)
+    def get_total_item_price(self):
+        return self.quantity*self.item.price
+    def get_total_item_discount_price(self):
+        return self.quantity*self.item.discount_price
+    def get_amount_saved(self):
+        return (self.item.price-self.item.discount_price)*self.quantity
+    def get_final_price(self):
+        if self.item.discount_price:
+            return self.get_total_item_discount_price()
+        return self.get_total_item_price()
 class Order(models.Model):
     
     user=models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -60,4 +70,12 @@ class Order(models.Model):
 
     def __str__(self):
         return self.user.username
+
+    def get_total_price(self):
+        total=0
+        for order_item in self.items.all():
+            total+= order_item.get_final_price()
+        return total
+
+
 
